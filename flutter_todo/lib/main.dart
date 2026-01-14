82818117c2d1d58f3bbcf208c27c97118515b837
Kahnishga <krishnakani19@gmail.com>
2026-01-13 22:54:18 -0600

    Updated app icon and title

--- flutter_todo/lib/main.dart
+++ flutter_todo/lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'ToDo',
       debugShowCheckedModeBanner: false,
       home: HomePage(),
       theme: ThemeData(primarySwatch: Colors.teal)
